9b0d6e244682e088a3faa5f7168d58965f63d15d
BIa <ana.ferreira@luizalabs.com>
2023-09-22 11:15:23 -0300

    codigo novo

---
+++ app.js
@@ -0,0 +1,13 @@
+  const firebaseConfig = {
+    apiKey: "AIzaSyAqv6B-HP0G_aRNf7KwepZyxNXzqwSIqRk",
+    authDomain: "authenticaton-900cb.firebaseapp.com",
+    projectId: "authenticaton-900cb",
+    storageBucket: "authenticaton-900cb.appspot.com",
+    messagingSenderId: "96469219054",
+    appId: "1:96469219054:web:f0669571471e21b61e25ff",
+    measurementId: "G-THKQ1HT5YE"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+ 
